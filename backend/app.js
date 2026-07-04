59f7ddf7d3bc9d59a5a3c4b6a9caa48704e37a7f
meti-0763 <daramade4@gmail.com>
2026-07-04 19:08:33 +0330

    files added

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-=======
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
->>>>>>> 41b23fb (new edit)
-require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-let app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,4 +17,3 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
->>>>>>> 7ba97f5 (.env added)
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-const app = require("./app")
+const app = require("./app") 
+require('dotenv').config();
+const mongoose = require("mongoose") 
+
+
+const port = process.env.PORT ;
+
+(async => {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("connected");
+    
+})();
+
+app.listen(port, ()=>{
+    console.log("connected")
+})
