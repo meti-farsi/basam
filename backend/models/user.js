fd64e7c448089533e2fbe1831f5b17acc79cd581
mahdi zakaleh <mahdizakaleh003@gmail.com>
2026-07-05 11:16:20 +0330

    model

---
+++ backend/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const schima = new mongoose.Schema({
+ username :{
+    type : String,
+    required : true
+ },
+ name :{
+    type : String,
+    required : true
+ },
+ email :{
+    type : String,
+    required : true,
+    unique : truse
+ },
+ password :{
+    type : String,
+    required : true
+ },
+ phone :{
+    type : String,
+    required : true
+ },
+ role:{
+    type :String ,
+    default : "USER",
+    required: true,
+    enum:["ADMIN" , USER]
+ }
+},
+{timestamps : true}
+);
+
+const model = mongoose.model("User",schima);
+
+module.exports = model
