d586c0f32b454a1bfdb8ea108d2b88943a82a444
meti-0763 <daramade4@gmail.com>
2026-07-05 11:17:06 +0330

    middleware

---
+++ backend/validators/userValidator.js
@@ -0,0 +1,18 @@
+const Validator = require("fastest-validator");
+
+const v = new Validator();
+
+const schema = {
+  name: { type: "string", min: 3, max: 255 },
+  username: { type: "string", min: 3, max: 255 },
+  email: { type: "email", min: 3, max: 255 },
+  phone: { type: "string", min: 3, max: 255 },
+  password: { type: "string", min: 3, max: 255 },
+  confirmPassword: { type: "equal",field :"password" },
+  role: { type: "string", min: 3, max: 255 },
+  $$srict:"true"
+};
+
+const check = v.compile(schema);
+
+madule.expoerts = check
