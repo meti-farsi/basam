c91c5422b7ab7ac6fc341f52b1424e67b78ecaaf
meti-0763 <daramade4@gmail.com>
2026-07-05 20:09:45 +0330

    finish

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -22,29 +22,29 @@ exports.register = async (req, res) => {
   });
 
   if (userExists) {
-    res.status(422).json({
+    return res.status(422).json({
       mass: "user hast",
     });
   }
   const isUserBan = await banUserModel.find({ phone });
 
-  if (isUserBan) {
-    res.status(409).json({
+  if (isUserBan.length) {
+   return res.status(409).json({
       message: "this phon number ban",
     });
   }
 
-  const userCount = await userModel.count();
-  const hashPassword = bcrypt.hash(password, 10);
+  const userCount = await userModel.countDocuments();
+  const hashPassword = await bcrypt.hash(password, 10);
 
   const user = await userModel.create({ username, name, email, password:hashPassword, phone , role:userCount > 0 ? "USER" : "ADMIN"});
   
 
-  accsesToken = jsonwebtoken.sign({id:user._id},process.env.JWT_SECRET,{
+  accsesToken =  jsonwebtoken.sign({id:user._id},process.env.JWT_SECRET,{
     expiresIn:"30 day"
   })
 
-  return res.status(201).json(user , accsesToken)
+  return res.status(201).json({user , accsesToken})
 
   
 };
