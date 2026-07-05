de9f4c1bd600ef8c4706dc4b78d349be9f6a3cdb
meti-0763 <daramade4@gmail.com>
2026-07-05 18:17:14 +0330

    accses token

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,55 +1,54 @@
-const userModel = require("../models/user")
-const banUserModel = require('../models/ban-phone')
-const registerValidator = require("../validators/userValidator")
+const userModel = require("../models/user");
+const banUserModel = require("../models/ban-phone");
+const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
+const registerValidator = require("../validators/userValidator");
 
-exports.register = async (req , res) =>{
+exports.register = async (req, res) => {
+  console.log(req.body);
 
-    console.log(req.body);
-    
-    const validateRigester = registerValidator(req.body)
+  const validateRigester = registerValidator(req.body);
 
+  console.log(validateRigester);
 
-    console.log(validateRigester);
-    
+  if (validateRigester !== true) {
+    return res.status(422).json(validateRigester);
+  }
 
-if(validateRigester !== true){
-    return res.status(422).json(validateRigester)
-}
+  let { username, name, email, password, phone } = req.body;
 
-let {username , name , email , password , phone } = req.body
+  const userExists = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
 
-
-const userExists = await userModel.findOne({
-    $or:[{username},{email}]
-})
-
-if(userExists){
+  if (userExists) {
     res.status(422).json({
-        mass : "user hast"
-    })
-} 
- const isUserBan = await banUserModel.find({phone})
+      mass: "user hast",
+    });
+  }
+  const isUserBan = await banUserModel.find({ phone });
 
- if(isUserBan){
+  if (isUserBan) {
     res.status(409).json({
-        message : 'this phon number ban'
-    })
- }
-    await userModel.create({username , name , email , password , phone })
-    res.status(201).json({
-        mass : "اوکیش شد "
-    })
-}
+      message: "this phon number ban",
+    });
+  }
 
+  const userCount = userModel.count();
+  const hashPassword = bcrypt.hash(password, 10);
 
+  const user = await userModel.create({ username, name, email, password:hashPassword, phone , role:userCount > 0 ? "USER" : "ADMIN"});
+  
 
+  accsesToken = jsonwebtoken.sign({id:user._id},process.env.JWT_SECRET,{
+    expiresIn:"30 day"
+  })
 
-exports.login = async (req , res) =>{
+  return res.status(201).json(user , accsesToken)
 
+  
+};
 
-}
+exports.login = async (req, res) => {};
 
-exports.getMe = async (req , res) =>{
-
-
-}
+exports.getMe = async (req, res) => {};
