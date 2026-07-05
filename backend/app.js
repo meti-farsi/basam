b77b10269e723a17c8790a9ede339abf74459ce3
mahdi zakaleh <mahdizakaleh003@gmail.com>
2026-07-05 11:39:03 +0330

    modelfdfo

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,11 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/auth');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/auth");
 const app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/users', usersRouter);
+const authRouter = require("./routes/");
 
 module.exports = app;
