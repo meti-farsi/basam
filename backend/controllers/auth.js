5fcfec9ec59468b21e5d3ffe59745cb51a0a1616
meti-0763 <daramade4@gmail.com>
2026-07-05 18:37:06 +0330

    accses token2

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -4,13 +4,6 @@ const bcrypt = require("bcrypt");
 const jsonwebtoken = require("jsonwebtoken");
 const registerValidator = require("../validators/userValidator");
 
-exports.register = async (req , res) =>{
-    
-    const validateRigester = registerValidator(req.body)
-
-if(validateRigester !== true){
-    return res.status(422).json(validateRigester)
-}
 exports.register = async (req, res) => {
   console.log(req.body);
 
@@ -24,28 +17,10 @@ exports.register = async (req, res) => {
 
   let { username, name, email, password, phone } = req.body;
 
+  const userExists = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
 
-const userExists = await userModel.findOne({
-    $or:[{username},{email}]
-})
-
-if(userExists){
-   return res.status(422).json({
-        mass : "user hast"
-    })
-} 
- const isUserBan = await banUserModel.find({phone : phone}).lean()
-
- if(isUserBan.length){
-   return res.status(409).json({
-        message : 'this phon number ban'
-    })
- }
-    await userModel.create({username , name , email , password , phone })
-    res.status(422).json({
-        mass : "اوکیش میکنم "
-    })
-}
   if (userExists) {
     res.status(422).json({
       mass: "user hast",
