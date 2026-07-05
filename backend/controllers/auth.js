9ac0ad1ac7d75cb0f81d893974a9b80a473c1492
mahdi zakaleh <mahdizakaleh003@gmail.com>
2026-07-05 18:18:25 +0330

    new change 2

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,14 +3,8 @@ const banUserModel = require('../models/ban-phone')
 const registerValidator = require("../validators/userValidator")
 
 exports.register = async (req , res) =>{
-
-    console.log(req.body);
     
     const validateRigester = registerValidator(req.body)
-
-
-    console.log(validateRigester);
-    
 
 if(validateRigester !== true){
     return res.status(422).json(validateRigester)
@@ -18,20 +12,19 @@ if(validateRigester !== true){
 
 let {username , name , email , password , phone } = req.body
 
-
 const userExists = await userModel.findOne({
     $or:[{username},{email}]
 })
 
 if(userExists){
-    res.status(422).json({
+   return res.status(422).json({
         mass : "user hast"
     })
 } 
- const isUserBan = await banUserModel.find({phone})
+ const isUserBan = await banUserModel.find({phone : phone}).lean()
 
- if(isUserBan){
-    res.status(409).json({
+ if(isUserBan.length){
+   return res.status(409).json({
         message : 'this phon number ban'
     })
  }
