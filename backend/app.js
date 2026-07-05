3708180fc7a172fca01880218b2a3759d0836846
mahdi zakaleh <mahdizakaleh003@gmail.com>
2026-07-05 11:53:18 +0330

    middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/auth");
+const cors = require("cors")
+const authRouter = require("./routes/auth");
+const bodyParser = require("body-parser");
 const app = express();
-const authRouter = require("./routes/");
+
+app.use('/courses/covers', express.static(path.join(__dirname, "public",'courses',"covers")))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
+
+
+app.use("/auth",authRouter)
 
 module.exports = app;
